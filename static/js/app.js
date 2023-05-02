9af05bafce2e392695f28464b44642578beb3e82
uzma-azhar <121704109+uzma-azhar@users.noreply.github.com>
2023-05-02 16:30:00 -0400

    Added gauge function

--- static/js/app.js
+++ static/js/app.js
@@ -19,6 +19,7 @@ function initialize()
         demoInfo(sample1) // function that pulls metadata
         buildBar(sample1) // function that builds bar chart
         buildBubble(sample1) // function that builds bubble chart
+        buildGauge(sample1) // function that build gauge chart
 
     });
 }
@@ -128,12 +129,59 @@ function buildBubble(sample)
     })
 }
 
+//build gauge 
+function buildGauge(sample)
+{
+    d3.json("static/js/samples.json").then((data) => {
+
+        let metaData = data.metadata; // pull meta data
+
+        // filter based on value of sample
+        let result = metaData.filter(sampleResult => sampleResult.id == sample);
+                
+        // access indexes from array to pull wash frequency
+        let washFrequency = result[0]['wfreq']
+        console.log(washFrequency)
+        
+        let gaugeChart = [
+            {
+                value: washFrequency,
+                type:"indicator",
+                mode: "gauge+number",
+                title: {text: "Belly Button Washing Frequency"},
+                gauge: {
+                    axis: { range: [0,9], visible: false},
+                    steps:[
+                        {range: [0,1], color: "#ffffe5"},
+                        {range: [1,2], color: "#f7fcb9"},
+                        {range: [2,3], color: "#d9f0a3"},
+                        {range: [3,4], color: "#addd8e"},
+                        {range: [4,5], color: "#78c679"},
+                        {range: [5,6], color: "#41ab5d"},
+                        {range: [6,7], color: "#238443"},
+                        {range: [7,8], color: "#006837"},
+                        {range: [8,9], color: "#004529"}
+
+                    ]
+                }
+            }
+        ];
+
+        let layout = { width: 600, height: 500, margin: { t: 0, b: 0 } }
+    
+        // draw the chart
+        Plotly.newPlot("gauge", gaugeChart, layout);
+
+    })
+}
+
 // update dashboard
 function optionChanged(item)
 {
     demoInfo(item); 
     buildBar(item);
     buildBubble(item);
+    buildGauge(item);
 }
 
 // call the function initialize
